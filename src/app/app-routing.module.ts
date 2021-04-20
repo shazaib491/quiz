5165386521c926e9c8999f3df75f55c0e11249e6
shazaib491 <shazaibrehman127@gmail.com>
2021-04-21 02:03:34 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResultComponent } from './result/result.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { SignupComponent } from './signup/signup.component';
+import { StartupComponent } from './startup/startup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'singup', component: SignupComponent },
+  { path: 'schedule', component: ScheduleComponent },
+  { path: 'startup', component: StartupComponent },
+  {path:'result',component:ResultComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { HomeComponent } from './home/home.component';
+import { StartupComponent } from './startup/startup.component';
+import { SignupComponent } from './signup/signup.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StartupComponent,
+    SignupComponent,
+    ScheduleComponent,
+    ResultComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
